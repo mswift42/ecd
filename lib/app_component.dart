5c6976fa81f37a3b7e13e1c080b03c624f5e58b3
mswift42 <martin.haesler@gmail.com>
2016-04-04 16:57:11 +0100

    add paper-toolbar for navbar

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,6 +2,14 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:angular2/core.dart';
+import 'package:polymer_elements/iron_flex_layout/classes/iron_flex_layout.dart';
+import 'package:polymer_elements/iron_icons.dart';
+import 'package:polymer_elements/paper_header_panel.dart';
+import 'package:polymer_elements/paper_toolbar.dart';
 
-@Component(selector: 'my-app', templateUrl: 'app_component.html')
+@Component(
+    selector: 'my-app',
+    templateUrl: 'app_component.html',
+    directives: const [],
+    encapsulation: ViewEncapsulation.Native)
 class AppComponent {}
